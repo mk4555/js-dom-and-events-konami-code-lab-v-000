d8acfbf0ef7315aa73d330ff4ceadcf51a020c6f
github username <github email address>
2018-04-09 13:45:13 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,22 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
+let index = 0
 function init() {
   // Write your JavaScript code inside the init() function
+  document.body.addEventListener('keydown', konamiCode)
+}
 
+
+function konamiCode(e) {
+  let key = parseInt(e.which || e.detail)
+
+  if (key === code[index]) {
+      index++
+      if (index === code.length) {
+        alert("TADA")
+        index = 0
+      }
+    }
+    else {
+      index = 0
+    }
 }
